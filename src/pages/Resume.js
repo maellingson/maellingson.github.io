764974aff04ddcc70d2d30c55e1797e030ea147c
Marissa <marissa.ellingson@gmail.com>
2018-09-19 12:02:14 -0500

    react pdf added

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -2,27 +2,56 @@ import React, { Component } from "react";
 import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
+import {Document, Page} from "react-pdf";
+import myResume from "../assets/Resume_Ellingson.pdf"
 
-const Resume = () => (
-    <div>
-    <Container style={{ marginTop: 30 }}>
-      <Row>
-        <Col size="md-12">
-          <h1>Resume</h1>
-        </Col>
-      </Row>
-      <Row>
-        <Col size="md-12">
-          <p>
-Resume goes here
-          </p>
 
-        </Col>
-      </Row>
-    </Container>
-  </div>
-);
+// const Resume = () => (
+//     <div>
+//     <Container style={{ marginTop: 30 }}>
+//       <Row>
+//         <Col size="md-12">
+//           <h1>Resume</h1>
+//         </Col>
+//       </Row>
+//       <Row>
+//         <Col size="md-12">
+//           <p>
+// Resume goes here
+//           </p>
 
+//         </Col>
+//       </Row>
+//     </Container>
+//   </div>
+// );
+
+class Resume extends Component {
+  state = {
+    numPages: 3,
+    pageNumber: 1,
+  }
+ 
+  onDocumentLoad = ({ numPages }) => {
+    this.setState({ numPages });
+  }
+ 
+  render() {
+    const { pageNumber, numPages } = this.state;
+ 
+    return (
+      <div>
+        <Document
+          file={myResume}
+          onLoadSuccess={this.onDocumentLoad}
+        >
+          <Page pageNumber={pageNumber} />
+        </Document>
+        <p>Page {pageNumber} of {numPages}</p>
+      </div>
+    );
+  }
+}
 
 
 
